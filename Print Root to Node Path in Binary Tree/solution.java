c242375d88e7e35c3a01a7296a1129c5c60e25a2
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-03-27 00:25:57 +0530

    Add files via upload

---
+++ Print Root to Node Path in Binary Tree/solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class solution {
+    public ArrayList<Integer> solve(TreeNode A, int B) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        if (A == null)
+            return arr;
+        getPath(A, arr, B);
+        return arr;
+    }
+
+    private boolean getPath(TreeNode root, ArrayList<Integer> arr, int x) {
+        if (root == null)
+            return false;
+        arr.add(root.val);
+        if (root.val == x)
+            return true;
+
+        if (getPath(root.left, arr, x) || getPath(root.right, arr, x))
+            return true;
+
+        arr.remove(arr.size() - 1);
+        return false;
+    }
+}
