8172555c926d2b78daceeae1a89c84c86f43f506
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-04-11 00:53:03 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (66.25%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -13,22 +13,16 @@
  *     }
  * }
  */
-class Solution 
-{
+class Solution {
+    TreeNode prev = null;
     public void flatten(TreeNode root) 
     {
-        TreeNode curr = root;
-        while(curr != null)
-        {
-            if(curr.left != null)
-            {
-                TreeNode prev = curr.left;
-                while(prev.right != null) prev = prev.right;
-                prev.right = curr.right;
-                curr.right = curr.left;
-            }
-            curr.left = null;
-            curr = curr.right;
-        }
+        if(root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
 }
