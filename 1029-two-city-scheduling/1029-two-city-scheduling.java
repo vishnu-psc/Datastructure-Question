c1d81cafcc336a5f101f3bd03b4f7ac30c1ce89d
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-07-03 12:58:49 +0530

    Time: 1 ms (99.13%), Space: 40.5 MB (89.91%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,18 +2,18 @@ class Solution
 {
     public int twoCitySchedCost(int[][] costs) 
     {
-        Arrays.sort(costs,(a,b) ->{
-            int diffa = a[0] - a[1];
-            int diffb = b[0] - b[1];
-            if(diffa == diffb) return a[0] - b[0];
-            else return diffa - diffb;
-        });    
+        int n = costs.length;
         int total = 0;
-        for(int i = 0; i < costs.length; i++)
+        int[] diff = new int[n];
+        for(int i = 0; i < n; i++)
         {
-            if(i < costs.length/2)
-                total += costs[i][0];
-            else total += costs[i][1];
+            total += costs[i][0];
+            diff[i] = costs[i][1] - costs[i][0];
+        }
+        Arrays.sort(diff);
+        for(int i = 0; i < n/2; i++)
+        {
+            total += diff[i];
         }
         return total;
     }
