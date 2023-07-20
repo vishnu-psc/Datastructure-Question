1ebfb542cee01aab2b8f083719f4bb376ff0a5ae
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-07-20 23:03:26 +0530

    Time: 12 ms (56.69%), Space: 43.9 MB (78.00%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,29 +2,28 @@ class Solution
 {
     public int[] asteroidCollision(int[] asteroids) 
     {
-        int n = asteroids.length;
-        if(n <= 1) return asteroids;
+        if(asteroids.length <= 1) return asteroids;
         
         Stack<Integer> st = new Stack<>();
-        for(int asteroid: asteroids)
+        
+        for(int stone: asteroids)
         {
-            if(asteroid > 0)
-                st.push(asteroid);
+            if(stone > 0) st.push(stone);
             else
             {
-                while(!st.isEmpty() && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(asteroid))
+                while(!st.isEmpty() && st.peek() > 0 && Math.abs(stone) > st.peek())
                     st.pop();
-            
-                if(st.isEmpty() || st.peek() < 0) st.push(asteroid);
-                else if(st.peek() == Math.abs(asteroid)) st.pop();
+                if(st.isEmpty() || st.peek() < 0) st.push(stone);
+                else if(st.peek() == Math.abs(stone)) st.pop();
             }
         }
-        int[] output = new int[st.size()];
+        
+        int[] ans = new int[st.size()]; 
+        
         int i = 0;
-        for(int element: st)
-        {
-            output[i++] = element;
-        }
-        return output;
+        for(int e: st)
+            ans[i++] = e;
+        
+        return ans;
     }
 }
