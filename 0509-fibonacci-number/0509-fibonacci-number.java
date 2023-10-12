a81ce0d1f95c037d42c0490424edda8a365a9bea
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-10-12 14:05:48 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (10.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -10,7 +10,11 @@ class Solution
     public int f(int n, int[] dp)
     {
         if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = fib(n-1) + fib(n-2);
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
