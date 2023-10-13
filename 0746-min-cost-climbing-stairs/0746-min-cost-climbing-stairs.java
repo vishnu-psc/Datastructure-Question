b6ba59db0f35dd6e77d9233babcee7259ec01f9a
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-10-14 01:13:14 +0530

    Time: 1 ms (46.18%), Space: 42.7 MB (75.32%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -5,15 +5,17 @@ class Solution
         int[] dp = new int[cost.length + 1];
         Arrays.fill(dp, -1);
         int n = cost.length;
-        return Math.min(solve(cost, n-1, dp), solve(cost, n-2, dp));    
+        return solve(cost, n, dp);  
     }
     
     public int solve(int[] cost, int n, int[] dp)
     {
-        if(n < 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int left = solve(cost, n-1, dp);
-        int right = solve(cost, n-2, dp);
-        return dp[n] = cost[n]+ Math.min(left, right);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < n; i++)
+        {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+        return Math.min(dp[n-1], dp[n-2]);
     }
 }
