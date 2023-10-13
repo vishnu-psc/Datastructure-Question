5a42ee17f8a03a113708a553f6d09998e1c93c4c
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-10-14 00:21:24 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (25.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,4 @@
-// Memoization
+// Tabulation
 class Solution 
 {
     public int climbStairs(int n) 
@@ -10,8 +10,12 @@ class Solution
     
     public int solve(int n, int[] dp)
     {
-        if(n == 0 || n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
