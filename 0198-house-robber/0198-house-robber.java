673e2ab33cdb46b28449c3047f8104597efcb7bc
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-11-09 00:09:06 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (53.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -11,15 +11,20 @@ class Solution
     
     public int solve(int[] nums, int n, int[] dp)
     {
-        dp[0] = nums[0];
+        int prev = nums[0];
+        int prev2 = 0, curr = 0;
+        
         for(int i = 1; i < n; i++)
         {
             int take = nums[i];
-            if(i > 1) take += dp[i-2];
-            int nottake = dp[i-1];
-            dp[i] = Math.max(take, nottake);
+            if(i > 1) take += prev2;
+            int nottake = prev;
+            curr = Math.max(take, nottake);
+            prev2 = prev;
+            prev = curr;
+            
         }
         
-        return dp[n-1];
+        return prev;
     }
 }
