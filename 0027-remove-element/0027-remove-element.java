83eb279da4e7809492685481e04e3d974b751929
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2023-12-01 00:04:42 +0530

    Time: 1 ms (7.62%), Space: 41.4 MB (11.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == val)
+            {
+                nums[i] = Integer.MAX_VALUE;
+                k++;
+            }
+        }
+        Arrays.sort(nums);
+        
+        return nums.length - k;
+    }
+}
