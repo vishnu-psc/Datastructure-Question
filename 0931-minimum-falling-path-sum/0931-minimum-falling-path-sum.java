a16d8dd6afdc651b190b5a0168f0ae4355d99647
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-01-20 01:23:21 +0530

    Time: 1 ms (99.27%), Space: 45.1 MB (48.81%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,33 @@
-class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+class Solution 
+{
+    public int minFallingPathSum(int[][] matrix) 
+    {
+        int n = matrix.length;
+        int m = matrix[0].length;
         int dp[][] = new int[m][n];
-        for(int j = 0; j < n; j++) {
-            dp[0][j] = matrix[0][j];
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++)
+        {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        for(int i = 1; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(j == 0) {
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j], dp[i-1][j+1]);
-                }
-                else if(j == n-1) {
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j], dp[i-1][j-1]);
-                }
-                else {
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i-1][j+1]));
-                }
-            }
+        for(int col = 0; col < m; col++)
+        {
+            min = Math.min(min, dfs(matrix, 0, col, dp));
         }
-        int res = Integer.MAX_VALUE;
-        for(int j = 0; j < n; j++) {
-            res = Math.min(dp[m-1][j], res);
-        }
-        return res;
+        return min;
+    }
+    
+    public int dfs(int[][] matrix, int row, int col, int[][] dp)
+    {
+        if(col < 0 || col >= matrix[0].length) return 101*100;
+        
+        if(row == matrix.length-1) return matrix[row][col];
+        if(dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
+        
+        int u = matrix[row][col] + dfs(matrix, row+1, col, dp);
+        int ld = matrix[row][col] + dfs(matrix, row+1, col-1, dp);
+        int rd = matrix[row][col] + dfs(matrix, row+1, col+1, dp);
+        
+        return dp[row][col] = Math.min(u, Math.min(ld, rd));
     }
 }
