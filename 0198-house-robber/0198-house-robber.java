5076723a1975c68e035dd6f9c7e809d2571b281d
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-01-22 00:55:30 +0530

    Time: 0 ms (100.00%), Space: 41 MB (31.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,21 @@
-// Memo
 class Solution 
 {
+    public int solve(int[] nums, int n, int[] dp)
+    {
+        if(n == 0) return nums[n];
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int pick = nums[n] + solve(nums, n-2, dp);
+        int leave = solve(nums, n-1, dp);
+        
+        return dp[n] = Math.max(pick, leave);
+    }
+    
     public int rob(int[] nums) 
     {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        return solve(nums, n, dp);
-    }
-    
-    public int solve(int[] nums, int n, int[] dp)
-    {
-        int prev = nums[0];
-        int prev2 = 0, curr = 0;
-        
-        for(int i = 1; i < n; i++)
-        {
-            int take = nums[i];
-            if(i > 1) take += prev2;
-            int nottake = prev;
-            curr = Math.max(take, nottake);
-            prev2 = prev;
-            prev = curr;
-            
-        }
-        
-        return prev;
+        return solve(nums, n-1, dp);  
     }
 }
