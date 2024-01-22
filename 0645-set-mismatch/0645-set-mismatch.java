332be05433b16e5cc793d0df573716bcad66992b
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-01-23 01:22:41 +0530

    Time: 3 ms (81.73%), Space: 45.5 MB (32.35%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,19 +1,45 @@
 class Solution 
 {
+    public boolean checkBit(int n, int i)
+    {
+        return ((n>>i) & 1) == 1;
+    }
+    
     public int[] findErrorNums(int[] nums) 
     {
         int n = nums.length;
         int duplicate = 0, missing = 0;
+        int xor = 0;
+        for(int i = 0; i < n; i++) xor = xor ^ nums[i] ^ (i+1);
         
-        Map<Integer, Integer> m = new HashMap<>();
-        for(int i = 1; i <= n; i++) m.put(i, 0);
-        
-        for(int i = 0; i < n; i++) m.put(nums[i], m.get(nums[i])-1);
-        
-        for(var i: m.entrySet())
+        int bitPos = 0;
+        for(int i = 0; i < 32; i++) 
         {
-            if(i.getValue() == -2) duplicate = i.getKey();
-            if(i.getValue() == 0) missing = i.getKey();
+            if(checkBit(xor, i))
+            {
+                bitPos = i;
+                break;
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(checkBit(nums[i], bitPos)) duplicate ^= nums[i];
+            else missing ^= nums[i];
+            
+            if(checkBit((i+1), bitPos)) duplicate ^= (i+1);
+            else missing ^= (i+1);
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == missing) 
+            {
+                int temp = duplicate;
+                duplicate = missing;
+                missing = temp;
+                break;
+            }
         }
         
         return new int[]{duplicate, missing};
