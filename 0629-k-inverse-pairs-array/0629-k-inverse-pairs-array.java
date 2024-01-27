d9b9fdd454a428aeb3628d4ec47bfd0f53471863
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-01-28 02:21:51 +0530

    Time: 1134 ms (36.36%), Space: 46.3 MB (55.37%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -2,28 +2,37 @@ class Solution
 {
     
     int mod = 1_000_000_007;
-    public int solve(int n, int k, int[][] dp)
-    {
-        if(n == 0) return 0;
-        if(k == 0) return 1;
-        
-        if(dp[n][k] != -1) return dp[n][k];
-        int result = 0;
-        
-        for(int inv = 0; inv <= Math.min(n-1,k); inv++)
-        {
-            result = (result + solve(n-1, k-inv, dp))%mod;
-        }
-        return dp[n][k] = result;
-    }
     
+//     public int solve(int n, int k, int[][] dp)
+//     {
+//         if(n == 0) return 0;
+//         if(k == 0) return 1;
+        
+//         if(dp[n][k] != -1) return dp[n][k];
+//         int result = 0;
+        
+//         for(int inv = 0; inv <= Math.min(n-1,k); inv++)
+//         {
+//             result = (result + solve(n-1, k-inv, dp))%mod;
+//         }
+//         return dp[n][k] = result;
+//     }
     public int kInversePairs(int n, int k) 
     {
         int[][] dp = new int[n+1][k+1];
-        for(int i = 0; i <= n; i++)
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        
+        for(int i = 1; i <= n; i++)
         {
-            Arrays.fill(dp[i], -1);
+            for(int j = 1; j <= k; j++)
+            {
+                for(int inv = 0; inv <= Math.min(i-1, j); inv++)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-inv])%mod;
+                }
+            }
         }
-        return solve(n, k, dp);
+        
+        return dp[n][k];
     }
 }
