2dd6ee9f9ba2da945c2e42df7fe327f156b6cf73
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-02-03 01:17:15 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (58.38%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) 
     {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i <= 8; i++)
-        {
-            q.add(i);
-        }
-        List<Integer> l = new ArrayList<>();
+        int[] arr = { 12, 23, 34, 45, 56, 67, 78, 89,
+                            123, 234, 345, 456, 567, 678, 789,
+                            1234, 2345, 3456, 4567, 5678, 6789,
+                            12345, 23456, 34567, 45678, 56789,
+                            123456, 234567, 345678, 456789,
+                            1234567, 2345678, 3456789,
+                            12345678, 23456789,
+                            123456789
+                          };    
         
-        while(!q.isEmpty())
+        List<Integer> l = new ArrayList<Integer>();
+        int n = arr.length;
+        
+        for(int i = 0; i <n; i++)
         {
-            int temp = q.peek();
-            q.poll();
-            if(temp >= low && temp <= high)
-            {
-                l.add(temp);
-            }
-            
-            int last = temp %10;
-            if(last + 1 < 10)
-            {
-                q.add(temp*10 + last+1);
-            }
+            if(arr[i] < low) continue;
+            if(arr[i] > high) break;
+            l.add(arr[i]);
         }
         return l;
-        
     }
 }
