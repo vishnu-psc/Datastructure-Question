92688d8efa75be87401317d9c3a81ba808cd4d66
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-03-07 01:02:51 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (87.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,17 +10,20 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) 
+    public boolean hasCycle(ListNode head)
     {
         if(head == null || head.next == null) return false;
-        ListNode slow = head;
         ListNode fast = head.next;
-        while(slow != fast)
+        ListNode slow = head;
+        
+        while(fast != slow)
         {
-            if(fast == null || fast.next == null) return false;
-            slow = slow.next;
+            if(fast == null || fast.next == null) return false;   
             fast = fast.next.next;
+            slow = slow.next;
         }
+        
         return true;
+        
     }
 }
