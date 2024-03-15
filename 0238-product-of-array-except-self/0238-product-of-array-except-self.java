8d5739bc5ec1284c74480b8278eb908951c0dd7d
vishnu-psc <95393451+vishnu-psc@users.noreply.github.com>
2024-03-16 02:59:12 +0530

    Time: 1 ms (100.00%), Space: 53 MB (77.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,24 @@
-class Solution {
-    public int[] productExceptSelf(int[] nums)
+class Solution 
+{
+    public int[] productExceptSelf(int[] nums) 
     {
-        int n = nums.length, temp = 1;    
-        int[] output = new int[n];
+        int n = nums.length;
+        int[] prefix = new int[n];
+        
+        int temp = 1;
+        
         for(int i = 0; i < n; i++)
         {
-            output[i] = temp;
+            prefix[i] = temp;
             temp = temp*nums[i];
         }
         temp = 1;
-        for(int i = n -1; i >= 0; i--)
+        
+        for(int i = n-1; i >= 0; i--)
         {
-            output[i] = temp*output[i];
+            prefix[i] = temp*prefix[i];
             temp = temp*nums[i];
         }
-        return output;
+        return prefix;
     }
 }
